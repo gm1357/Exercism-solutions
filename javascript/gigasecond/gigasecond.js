11fbdc9b8f93779b83f09172c9ac1fdb6c7a4867
Gabriel Machado <g_machado1@hotmail.com>
2019-02-19 15:49:06 -0300

    Refatorado "Gigasecond" em Javascript

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,3 +1,5 @@
+const GIGASECOND_IN_MS = (10 ** 9) * 1000
+
 export const gigasecond =  (initialDate) => {
-    return new Date(initialDate.getTime() + 1000 * (10 ** 9));
+    return new Date(initialDate.getTime() + GIGASECOND_IN_MS);
 };
