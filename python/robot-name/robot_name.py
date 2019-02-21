1283aede412e4693bb7f2a2cddb608d2f632155e
Gabriel Machado <g_machado1@hotmail.com>
2019-02-21 19:15:46 -0300

    Refatorado "Robot Name" em Python

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -7,15 +7,6 @@ class Robot(object):
     def __generate_name(self):
         return "".join(random.choice(string.ascii_uppercase) for _ in range(2)) + "".join(random.choice(string.digits) for _ in range(3))
 
-    def __init__(self):
-        new_name = self.__generate_name()
-
-        if new_name not in self.names:
-            self.name = new_name
-            self.names.append(new_name)
-        else:
-            self.reset()
-
     def reset(self):
         new_name = self.__generate_name()
         
@@ -24,3 +15,6 @@ class Robot(object):
             self.names.append(new_name)
         else:
             self.reset()
+
+    def __init__(self):
+        self.reset()
