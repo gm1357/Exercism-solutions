2d4ee8bf7b362951760414f887e0e807c30c3be3
gm1357 <g_machado1@hotmail.com>
2019-08-23 09:20:16 -0300

    Concluído "Error Handling" em Bash

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+main () {
+    if [ "$#" -ne  "1" ]; then
+        echo "Usage: ./error_handling <greetee>"
+        exit 1
+    fi
+
+    echo "Hello, $1"
+    exit 0
+}
+
+main "$@"
