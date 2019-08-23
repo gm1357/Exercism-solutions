43229b47c92d2390cc2d9003464040fc7bb1273d
gm1357 <g_machado1@hotmail.com>
2019-08-23 14:05:41 -0300

    Refatorado "Pangram" em Kotlin

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,11 +1,3 @@
 object Pangram {
-    
-    fun isPangram(sentence: String): Boolean {
-        val sentenceLowered = sentence.toLowerCase();
-
-        for (letter in 'a'..'z')
-            when { letter !in sentenceLowered -> return false; }
-
-        return true;
-    }
+    fun isPangram(sentence: String) = ('a'..'z').all { it in sentence.toLowerCase() }
 }
