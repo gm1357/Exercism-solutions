22691029334fb1166558a18a7c9aadb6043696d5
gm1357 <g_machado1@hotmail.com>
2019-08-27 09:58:02 -0300

    Concluído "Raindrops" em Bash

---
+++ bash/raindrops/raindrops.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+
+main () {
+    if [ "$#" -ne  "1" ]; then
+        echo "Usage: ./`basename $0` <number>"
+        exit 1
+    fi
+
+    raindrop_string=""
+
+    build_raindrop "$@"
+
+    echo "$raindrop_string"
+}
+
+build_raindrop() {
+
+    check_factor $1 3 "Pling"
+    check_factor $1 5 "Plang"
+    check_factor $1 7 "Plong"
+
+    if [[ -z "$raindrop_string" ]]; then
+        raindrop_string="$1"
+    fi
+}
+
+check_factor() {
+    if [[ `expr $1 % $2` -eq "0" ]]; then
+        raindrop_string="${raindrop_string}$3"
+    fi
+}
+
+main "$@"
