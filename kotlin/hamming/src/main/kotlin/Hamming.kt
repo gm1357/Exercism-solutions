1b5c0e370122bc4f1afa1da8b4b684a1fc6647eb
gm1357 <g_machado1@hotmail.com>
2019-08-27 10:16:17 -0300

    Refatorado "Hamming" em Kotlin

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,6 +3,6 @@ object Hamming {
     fun compute(leftStrand: String, rightStrand: String): Int {
         require (leftStrand.length == rightStrand.length) { "left and right strands must be of equal length." }
 
-        return leftStrand.filterIndexed { index, char -> char != rightStrand[index] }.length
+        return leftStrand.zip(rightStrand).filter { it.first != it.second }.count()
     }
 }
