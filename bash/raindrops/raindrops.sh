a6219a7bffcc4a174178a193f92fcfb011c66b7b
gm1357 <g_machado1@hotmail.com>
2019-08-28 11:27:02 -0300

    Refatorado "Raindrops" em Bash

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -7,27 +7,18 @@ main () {
         exit 1
     fi
 
-    raindrop_string=""
+    raindrop_string=$(build_raindrop "$@")
 
-    build_raindrop "$@"
-
-    echo "$raindrop_string"
+    echo "${raindrop_string:-$1}"
 }
 
 build_raindrop() {
-
-    check_factor "$1" 3 "Pling"
-    check_factor "$1" 5 "Plang"
-    check_factor "$1" 7 "Plong"
-
-    if [[ -z "$raindrop_string" ]]; then
-        raindrop_string="$1"
-    fi
+    echo "$(check_factor "$1" 3 "Pling")$(check_factor "$1" 5 "Plang")$(check_factor "$1" 7 "Plong")"
 }
 
 check_factor() {
     if (( "$1" % "$2" == 0 )); then
-        raindrop_string="${raindrop_string}$3"
+        echo "$3"
     fi
 }
 
