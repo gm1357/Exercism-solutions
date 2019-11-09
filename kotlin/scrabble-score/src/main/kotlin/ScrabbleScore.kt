d0970dd61c985cc778df5073f43f85cfb2a09d1a
Gabriel Machado <g_machado1@hotmail.com>
2019-11-09 19:36:03 -0300

    Refatorado "Scrabble Score" em Kotlin

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,19 +1,16 @@
 object ScrabbleScore {
 
-    private val lettersValues = hashMapOf(
-            "aeioulnrst" to 1,
-            "dg" to 2,
-            "bcmp" to 3,
-            "fhvwy" to 4,
-            "k" to 5,
-            "jx" to 8,
-            "qz" to 10
-    )
+    private val lettersValues = "aeioulnrst".associate {it to 1}
+            .plus("dg".associate {it to 2})
+            .plus("bcmp".associate {it to 3})
+            .plus("fhvwy".associate {it to 4})
+            .plus("k".associate {it to 5})
+            .plus("jx".associate {it to 8})
+            .plus("qz".associate {it to 10})
 
     fun scoreWord(word: String): Int {
         return word.map {
-            letter -> lettersValues.filter { entry -> entry.key.contains(letter.toLowerCase()) }.values.sum()
-        }.sum()
+            letter -> if (lettersValues.containsKey(letter.toLowerCase())) lettersValues[letter.toLowerCase()] else 0
+        }.sumBy { it!! }
     }
-
 }
